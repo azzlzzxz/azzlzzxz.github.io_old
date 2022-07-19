29f62c284ac8368835110ae99e4ef283cc112d03
azzlzzxz <37786423+azzlzzxz@users.noreply.github.com>
2022-07-19 19:29:06 +0800

    Deploying to gh-pages from  @ dffaec2e3515a120a973bd530e4df473f29b6da6 🚀

---
+++ assets/ts.html.51c4a87c.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-068c64e9","path":"/typescript/ts.html","title":"","lang":"zh-CN","frontmatter":{},"excerpt":"","headers":[{"level":2,"title":"TypeScript","slug":"typescript","children":[]}],"git":{"updatedTime":1658230092000,"contributors":[{"name":"xinxu","email":"azzlzzxz@gmail.com","commits":1}]},"filePathRelative":"typescript/ts.md"}');export{t as data};
