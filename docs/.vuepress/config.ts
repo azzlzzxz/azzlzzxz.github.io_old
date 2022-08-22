e6ff9b484c32decc415e380eee3751b714327c25
xinxu <azzlzzxz@gmail.com>
2022-08-22 22:44:58 +0800

    feat: 修改主页配置文件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: xinxu
  * @Date: 2022-06-29 15:51:52
  * @LastEditors: xinxu
- * @LastEditTime: 2022-08-18 10:58:00
+ * @LastEditTime: 2022-08-22 22:44:24
  * @FilePath: /azzlzzxz.github.io/docs/.vuepress/config.ts
  */
 import { defineUserConfig } from "vuepress";
@@ -11,10 +11,11 @@ const { defaultTheme } = require("@vuepress/theme-default");
 
 export default defineUserConfig({
   lang: "zh-CN",
-  title: "Steins Gate 主页",
+  title: "Steins Gate",
   description: "Steins Gate的前端记录",
   head: [["link", { rel: "icon", href: "/logo.png" }]],
   theme: defaultTheme({
+    logo: "/logo.png",
     navbar: [
       {
         text: "前端框架",
@@ -28,6 +29,10 @@ export default defineUserConfig({
       {
         text: "TS",
         link: "/typescript/ts.md",
+      },
+      {
+        text: "主页",
+        link: "https://github.com/azzlzzxz",
       },
     ],
     sidebar: {
--- docs/README.md
+++ docs/README.md
@@ -4,14 +4,14 @@ heroImage: /logo.png
 heroText: Steins Gate的前端记录
 tagline: 一点一滴都是进步
 actions:
-  - text: 马上进入 →
+  - text: 前端知识
     link: /promise/promise.md
-    type: primary 
+    type: primary
 features:
-- title: 学习记录
-  details: 平时学习技术的总结和记录。
-- title: 工作记录
-  details: 平时工作遇到的问题。
-- title: 生活记录
-  details: 生活中有趣的事情。
+  - title: 学习记录
+    details: 平时学习技术的总结和记录。
+  - title: 工作记录
+    details: 平时工作遇到的问题。
+  - title: 生活记录
+    details: 生活中有趣的事情。
 ---
